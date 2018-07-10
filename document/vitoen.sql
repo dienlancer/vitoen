5dda4e3d376ad05b75f664baf3291eb976bf5156
NguyenKimDien <trietnk01@gmail.com>
2018-07-10 17:28:49 +0700

    cập nhật

--- document/vitoen.sql
+++ document/vitoen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 10, 2018 at 12:25 PM
+-- Generation Time: Jul 10, 2018 at 12:28 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.1.17
 
