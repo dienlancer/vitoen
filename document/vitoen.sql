7bd45fa35482c0405d6386d27c8bd031652820e9
NguyenKimDien <trietnk01@gmail.com>
2018-07-10 23:08:59 +0700

    cap nhat

--- document/vitoen.sql
+++ document/vitoen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 10, 2018 at 06:06 PM
+-- Generation Time: Jul 10, 2018 at 06:08 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.1.17
 
