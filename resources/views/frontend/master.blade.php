c040e75c6076b42ddb0cc7e1b48fa14a682a1c94
NguyenKimDien <trietnk01@gmail.com>
2018-07-11 12:46:27 +0700

    cập nhật

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -777,7 +777,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/5ad8d8b4227d3d7edc241f50/default';
+s1.src='https://embed.tawk.to/5b4596744af8e57442dc8525/default';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
